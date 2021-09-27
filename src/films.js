f0ba1fdd69964291a19281784a70168e824f4171
John Doe <johndoe@example.com>
2021-09-27 10:14:49 +0200

    typescript done

--- src/films.js
+++ src/films.js
@@ -1,22 +1,21 @@
 "use strict";
-let result;
 // Exercise 1: Get the array of all directors.
-const getAllDirectors = (movies) => result = [...movies.map((movie) => movie.director)];
+const getAllDirectors = (movies) => [...movies.map((movie) => movie.director)];
 // Exercise 2: Get the films of a certain director
-const getMoviesFromDirector = (movies, searcheddirector) => result = [...movies.filter((movie) => movie.director === searcheddirector)];
+const getMoviesFromDirector = (movies, searcheddirector) => [...movies.filter((movie) => movie.director === searcheddirector)];
 // Exercise 3: Calculate the average of the films of a given director.
 const moviesAverageOfDirector = (movies, searcheddirector) => {
     let scores = getMoviesFromDirector(movies, searcheddirector).map((movie) => movie.score);
-    return result = scores.reduce((a, b) => a + b) / scores.length;
+    return scores.reduce((a, b) => a + b) / scores.length;
 };
 // Exercise 4:  Alphabetic order by title 
-const orderAlphabetically = (movies) => result = [...movies.map((movie) => movie.title)].sort().slice(0, 20);
+const orderAlphabetically = (movies) => [...movies.map((movie) => movie.title)].sort().slice(0, 20);
 // Exercise 5: Order by year, ascending
-const orderByYear = (movies) => result = [...movies.sort((a, b) => a.year > b.year ? 1 : -1)];
+const orderByYear = (movies) => [...movies.sort((a, b) => a.year > b.year ? 1 : -1)];
 // Exercise 6: Calculate the average of the movies in a category
 const moviesAverageByCategory = (movies, searchedGenre) => {
-    let scores = [...movies.filter((movie) => movie.genre.includes(searchedGenre) && movie.score !== '')].map((movie) => movie.score);
-    return result = scores.reduce((a, b) => a + b) / scores.length;
+    let scores = [...movies.filter((movie) => movie.genre.includes(searchedGenre) && typeof movie.score !== 'string')].map((movie) => movie.score);
+    return scores.reduce((a, b) => a + b) / scores.length;
 };
 // Exercise 7: Modify the duration of movies to minutes
 const hoursToMinutes = (movies) => {
@@ -37,7 +36,7 @@ const hoursToMinutes = (movies) => {
     return result;
 };
 // Exercise 8: Get the best film of a year
-const bestFilmOfYear = (movies, searchedYear) => result = movies.filter((movie) => movie.year === searchedYear).sort((a, b) => a.score < b.score ? 1 : -1).slice(0, 1);
+const bestFilmOfYear = (movies, searchedYear) => movies.filter((movie) => movie.year === searchedYear).sort((a, b) => a.score < b.score ? 1 : -1).slice(0, 1);
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
--- src/ts/films.ts
+++ src/ts/films.ts
@@ -1,39 +1,38 @@
-let result: object[] | string[] | number
 type movie = {
   director: string
   title: string
-  score: any
+  score: number
   genre: string[]
   duration: string
   year: number
 }
 
 // Exercise 1: Get the array of all directors.
-const getAllDirectors = (movies: []) => result = [...movies.map((movie: movie) => movie.director)]
+const getAllDirectors = (movies: []) => [...movies.map((movie: movie) => movie.director)]
 
 
 // Exercise 2: Get the films of a certain director
-const getMoviesFromDirector = (movies: [], searcheddirector: string) => result = [...movies.filter((movie: movie) => movie.director === searcheddirector)]
+const getMoviesFromDirector = (movies: [], searcheddirector: string) => [...movies.filter((movie: movie) => movie.director === searcheddirector)]
 
 
 // Exercise 3: Calculate the average of the films of a given director.
 const moviesAverageOfDirector = (movies: [], searcheddirector: string) => {
   let scores = getMoviesFromDirector(movies, searcheddirector).map((movie: movie) => movie.score)
-  return result = scores.reduce((a, b) => a + b) / scores.length
+  return scores.reduce((a, b) => a + b) / scores.length
 }
 
 // Exercise 4:  Alphabetic order by title 
-const orderAlphabetically = (movies: []) => result = [...movies.map((movie: movie) => movie.title)].sort().slice(0, 20)
+const orderAlphabetically = (movies: []) => [...movies.map((movie: movie) => movie.title)].sort().slice(0, 20)
 
 
 // Exercise 5: Order by year, ascending
-const orderByYear = (movies: []) => result = [...movies.sort((a: movie, b: movie) => a.year > b.year ? 1 : -1)]
+const orderByYear = (movies: []) => [...movies.sort((a: movie, b: movie) => a.year > b.year ? 1 : -1)]
 
 
 // Exercise 6: Calculate the average of the movies in a category
 const moviesAverageByCategory = (movies: [], searchedGenre: string) => {
-  let scores = [...movies.filter((movie: movie) => movie.genre.includes(searchedGenre) && movie.score !== '')].map((movie: movie) => movie.score)
-  return result = scores.reduce((a, b) => a + b) / scores.length
+  let scores = [...movies.filter((movie: movie) => movie.genre.includes(searchedGenre) && typeof movie.score !== 'string')].map((movie: movie) => movie.score)
+  return scores.reduce((a, b) => a + b) / scores.length
 }
 
 // Exercise 7: Modify the duration of movies to minutes
@@ -60,7 +59,7 @@ const hoursToMinutes = (movies: []) => {
 }
 
 // Exercise 8: Get the best film of a year
-const bestFilmOfYear = (movies: [], searchedYear: number) => result = movies.filter((movie: movie) => movie.year === searchedYear).sort((a: movie, b: movie) => a.score < b.score ? 1 : -1).slice(0, 1)
+const bestFilmOfYear = (movies: [], searchedYear: number) => movies.filter((movie: movie) => movie.year === searchedYear).sort((a: movie, b: movie) => a.score < b.score ? 1 : -1).slice(0, 1)
 
 
 
